1f9059f8f70d9d7bf718019b45757ed5c3909b30
Swayam-555 <79014200+Swayam-555@users.noreply.github.com>
2021-03-30 12:34:29 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 var canvas;
 var music;
 var surface1,surface2,surface3,surface4;
+var box;
 
 function preload(){
     music = loadSound("music.mp3");
@@ -23,19 +24,63 @@ var box_options = {
     restitution = 1
 }
     //create box sprite and give velocity
-     box = createSprite(400,100,50,50,box_options);
-     box.velocityX = -3;
-
+     box = createSprite(random(20,750),100,50,50,box_options);
+     box.velocityX = 3;
+box.shapeColor = "gray"
      
 
 }
 
 function draw() {
-    background("gray");
+    background("black");
+
+    
+
     //create edgeSprite
       createEdgeSprites();
       box.collide(edges)
 
 
     //add condition to check if box touching surface and make bounce.
+    if(isTouching(box,surface1)){
+      surface1.shapeColor = "green";
+    box.shapeColor = surface1.shapeColor;
+    music.play;
+    }else{
+      surface1.shapeColor = "green";
+      box.shapeColor = "gray";
+      music.stop;
+    }
+
+    if(isTouching(box,surface2)){
+      surface.shapeColor = "yellow";
+    box.shapeColor = surface2.shapeColor;
+    music.play;
+    }else{
+      surface2.shapeColor = "yellow";
+      box.shapeColor = "gray";
+      music.stop;
+    }
+
+    if(isTouching(box,surface3)){
+      surface3.shapeColor = "blue";
+    box.shapeColor = surface3.shapeColor;
+    music.play;
+    }else{
+      surface3.shapeColor = "blue";
+      box.shapeColor = "gray";
+      music.stop;
+    }
+
+    if(isTouching(box,surface4)){
+      surface4.shapeColor = "red";
+    box.shapeColor = surface4.shapeColor;
+    music.play;
+    }else{
+      surface4.shapeColor = "red";
+      box.shapeColor = "gray";
+      music.stop
+    }
+
+    drawSprites();
 }
