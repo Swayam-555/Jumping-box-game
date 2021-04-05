04d1c568d8baee8358b8f78e08f0732eb233d445
Swayam-555 <79014200+Swayam-555@users.noreply.github.com>
2021-04-05 21:51:37 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -15,11 +15,11 @@ function setup(){
    surface1 = createSprite(400,300,100,100);
    surface1.shapeColor = "green";
    surface2 = createSprite(400,300,100,100);
-surface2.ShapeColor = "yellow"
+surface2.shapeColor = "yellow"
    surface3 = createSprite(400,300,100,100);
-surface3.ShapeColor = "blue"
+surface3.shapeColor = "blue"
    surface4 = createSprite(400,300,100,100);
-surface4.ShapeColor = "red"
+surface4.shapeColor = "red"
 var box_options = {
     restitution = 1
 }
@@ -37,49 +37,49 @@ function draw() {
     
 
     //create edgeSprite
-      createEdgeSprites();
-      box.collide(edges)
-
+     edges = createEdgeSprites();
+      box.collide(edges);
+     box.bounceOff(edges);
 
     //add condition to check if box touching surface and make bounce.
-    if(isTouching(box,surface1)){
+    if(isTouching(box,surface1)&& box.bounceOff(surface1)){
       surface1.shapeColor = "green";
     box.shapeColor = surface1.shapeColor;
-   // music.play;
+   music.play();
     }else{
       surface1.shapeColor = "green";
       box.shapeColor = "gray";
-     // music.stop;
+      music.stop();
     }
 
-    if(isTouching(box,surface2)){
+    if(isTouching(box,surface2)&& box.bounceOff(surface1)){
       surface.shapeColor = "yellow";
     box.shapeColor = surface2.shapeColor;
-   // music.play;
+    music.play();
     }else{
       surface2.shapeColor = "yellow";
       box.shapeColor = "gray";
-     // music.stop;
+      music.stop();
     }
 
-    if(isTouching(box,surface3)){
+    if(isTouching(box,surface3)&& box.bounceOff(surface1)){
       surface3.shapeColor = "blue";
     box.shapeColor = surface3.shapeColor;
-   // music.play;
+    music.play();
     }else{
       surface3.shapeColor = "blue";
       box.shapeColor = "gray";
-     // music.stop;
+      music.stop();
     }
 
-    if(isTouching(box,surface4)){
+    if(isTouching(box,surface4)&& box.bounceOff(surface1)){
       surface4.shapeColor = "red";
     box.shapeColor = surface4.shapeColor;
-    //music.play;
+    music.play();
     }else{
       surface4.shapeColor = "red";
       box.shapeColor = "gray";
-     // music.stop;
+      music.stop();
     }
 
     drawSprites();
